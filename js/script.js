f399cb789af9d15eea59f874eaf2adca98bba397
Mohamad Rizkon Nurkholis <rizkon86system@gmail.com>
2023-06-23 16:11:29 +0700

    updatean

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
-// toggle class aktif
+// toggle class aktif untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 // ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// klik di luar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector("#hamburger-menu");
+// Toggle class active untuk search form
+const searchform = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchform.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// klik di luar elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchform.contains(e.target)) {
+    searchform.classList.remove("active");
   }
 });
