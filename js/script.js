c67a8699305980f321350443c5c35a3e9c2dd23b
Mohamad Rizkon Nurkholis <rizkon86system@gmail.com>
2023-06-24 14:55:44 +0700

    memperbaiki heheheh

--- js/script.js
+++ js/script.js
@@ -15,9 +15,17 @@ document.querySelector("#search-button").onclick = (e) => {
   e.preventDefault();
 };
 
+// toggle class active untuk shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-button").onclick = () => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
 // klik di luar elemen
 const hm = document.querySelector("#hamburger-menu");
 const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
 
 document.addEventListener("click", function (e) {
   if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
@@ -26,4 +34,31 @@ document.addEventListener("click", function (e) {
   if (!sb.contains(e.target) && !searchform.contains(e.target)) {
     searchform.classList.remove("active");
   }
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+// Modal Box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// klik tombol close
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+// klik diluar modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    modal.style.display = "none";
+  }
+};
